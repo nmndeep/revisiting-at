50e84b3e6c4f27cc7c950abd45523a8395edc0be
nmndeep <nmndeep@gmail.com>
2023-02-22 10:32:27 +0100

    cleanup

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES='DEVICE_IDS separated by comma'
 
 sleep 2s
 
@@ -11,10 +11,10 @@ python3 main.py --data.num_workers=12 --data.in_memory=1 \
         --data.train_dataset=path-to-imagenet-train-set \
         --data.val_dataset=path-to-imagenet-val-set \
         --logging.folder=path-to-logging-folder --logging.log_level 2 \
-    --adv.attack apgd --adv.n_iter 2 --adv.norm L2 --training.distributed 1 --training.batch_size 80 --validation.batch_size 16 --lr.lr 1e-3 --logging.save_freq 2  \
+    --adv.attack apgd --adv.n_iter 2 --adv.norm Linf --training.distributed 1 --training.batch_size 80 --validation.batch_size 16 --lr.lr 1e-3 --logging.save_freq 2  \
     --resolution.min_res 224 --resolution.max_res 224 --data.seed 0 --data.augmentations 1 --model.add_normalization 0\
-    --model.updated 0 --model.not_original 1 --model.model_ema 1 --lr.lr_peak_epoch 5\
+    --model.updated 0 --model.not_original 1 --model.model_ema 1 --lr.lr_peak_epoch 20\
     --training.label_smoothing 0.1 --logging.addendum='additional_text_appended_to_save_folder_name'\
-    --dist.world_size 8 --training.distributed 1 --model.pretrained 0\
-    --model.arch convnext_base --training.epochs 20 \
+    --dist.world_size '# of GPUS' --training.distributed 1 --model.pretrained 0\
+    --model.arch convnext_base --training.epochs 300 \
    
