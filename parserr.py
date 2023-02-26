86c74ee31bfc3912bbc19c448d938ea356636eaa
nmndeep <nmndeep@gmail.com>
2023-02-26 20:15:37 +0100

    Update parserr.py

--- parserr.py
+++ parserr.py
@@ -4,10 +4,7 @@
 import argparse
 
 def str2bool(v):
-    """
-    Converts string to bool type; enables command line 
-    arguments in the format of '--arg1 true --arg2 false'
-    """
+
     if isinstance(v, bool):
         return v
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
